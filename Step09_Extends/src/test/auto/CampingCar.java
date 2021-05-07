832dbe3c1e008802cd74370f0157ddaa08085a51
DESKTOP-OGKSF7E\acorn <seolhwacode@gmail.com>
2021-05-07 12:26:55 +0900

    Step09 CampingCar.java add

---
+++ Step09_Extends/src/test/auto/CampingCar.java
@@ -0,0 +1,15 @@
+package test.auto;
+
+public class CampingCar extends Car {
+
+	public CampingCar(Engine engine) {
+		super(engine);
+		System.out.println("CampingCar의 생성자입니다.");
+	}
+	
+	//메소드
+	public void doCamping() {
+		
+	}
+	
+}
