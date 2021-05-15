259dbf854d9950c782c540f305a0536a5a3c697f
DESKTOP-OGKSF7E\acorn <seolhwacode@gmail.com>
2021-05-15 23:39:24 +0900

    file edit

--- Step19_JDBC/src/test/main/MainClass10.java
+++ Step19_JDBC/src/test/main/MainClass10.java
@@ -11,16 +11,16 @@ public class MainClass10 {
 		//static 메소드를 이용해서 MemberDao 객체의 참조값을 받아올수 있다.
 		MemberDao dao=MemberDao.getInstance();
 		//새로 추가할 회원의 정보라면 
-//		MemberDto dto=new MemberDto();
-//		dto.setName("덩어리");
-//		dto.setAddr("상도동");
-//		//MemberDao 객체의 메소드를 활용해서 저장할수 있다. 
-//		dao.insert(dto);
+		MemberDto dto=new MemberDto();
+		dto.setName("덩어리");
+		dto.setAddr("상도동");
+		//MemberDao 객체의 메소드를 활용해서 저장할수 있다. 
+		dao.insert(dto);
 		
 		MemberDao dao2=MemberDao.getInstance();
 		//수정할 회원의 정보라면
-//		MemberDto dto2=new MemberDto(1, "김구라", "독산동");
-//		dao2.update(dto2);
+		MemberDto dto2=new MemberDto(1, "김구라", "독산동");
+		dao2.update(dto2);
 		
 	}
 }
